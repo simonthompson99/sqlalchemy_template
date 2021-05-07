c1058ddd65cbf8e659554cbdad7dd9293ce7e2ab
Simon Thompson <simonthompson99@googlemail.com>
2021-05-07 16:47:23 +0100

    Make use of new create_database function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ which provides a commandline structure to the functions.
 import logging
 import fire
 from config import Config
-from models import get_engine
+from models import create_database
 from modules import log
 
 LOGGER = logging.getLogger(__name__)
@@ -18,16 +18,11 @@ class Main():
 
         def db_a(self):
 
-            LOGGER.info('here')
+            LOGGER.info('creating db_a')
 
             c = Config()
 
-            e = get_engine(c.get_conn_str('db_a'))
-            LOGGER.info(c.get_conn_str('db_a'))
-
-            m = c.get_db_model('db_a')
-
-            m.Base.metadata.create_all(e)
+            create_database(c, 'db_a')
 
 
 if __name__ == "__main__":
